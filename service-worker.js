c3218940c43e0238c907266a8dee07edd1ce1ca1
nonna-tech <receitasnonnaangela1@gmail.com>
2025-10-20 12:46:32 -0300

    Update service worker cache version and assets

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,26 @@
-const CACHE = 'medichora-v1';
+const CACHE = 'medichora-v2';
 const ASSETS = [
-  '/', '/index.html','/cadastro.html','/sobre.html',
-  '/css/estilo.css',
-  '/js/app.js','/js/storage.js','/js/pwa.js','/js/cadastro.js',
-  '/manifest.json',
-  '/assets/sounds/som_alerta.wav'
+  'index.html','cadastro.html','sobre.html',
+  'css/estilo.css',
+  'js/app.js','js/storage.js','js/pwa.js','js/cadastro.js',
+  'manifest.json',
+  'assets/sounds/som_alerta.wav'
 ];
+
 self.addEventListener('install', e=>{
   e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
 });
+
 self.addEventListener('activate', e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
 });
+
 self.addEventListener('fetch', e=>{
-  const url = new URL(e.request.url);
-  if(url.origin===location.origin){
-    e.respondWith(caches.match(e.request).then(r=> r || fetch(e.request)));
-  } else {
-    e.respondWith(fetch(e.request));
-  }
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
 });
